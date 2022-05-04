e016ead52a7ec45a7c00e0646debadd8fcb77111
AnxhelaMehmetaj <45404657+AnxhelaMehmetaj@users.noreply.github.com>
2022-05-04 00:28:43 -0400

    Revert "backwards "

--- app/auth/decorators/__init__.py
+++ app/auth/decorators/__init__.py
@@ -2,7 +2,6 @@ from functools import wraps
 from flask_login import current_user
 from flask import render_template
 
-
 def admin_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
